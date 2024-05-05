fbcda1db5d119db61098190285e720f09ad86ca8
Arthur P L S Santos <arthurpedroweb@gmail.com>
2024-05-04 21:45:50 -0300

    feat: Adicionando a configuração inicial do electron

---
+++ web/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1080,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/web/browser/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
